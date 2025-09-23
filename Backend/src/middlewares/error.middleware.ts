521c04ec816ef19a730d9ca822b0c5651d24a1ce
jesus1102 <jesusdavidgonzales1102@gmail.com>
2025-09-23 15:49:41 -0500

    feat(config): archivos de configuracion y esenciales

---
+++ Backend/src/middlewares/error.middleware.ts
@@ -0,0 +1,16 @@
+import { Request, Response, NextFunction } from 'express';
+
+const errorHandler = (err: any, _req: Request, res: Response, _next: NextFunction) => {
+  console.error('❌ Error:', err);
+
+  const status = err.status || 500;
+  const message = err.message || 'Internal Server Error';
+
+  res.status(status).json({
+    success: false,
+    message,
+    ...(process.env.NODE_ENV !== 'prod' && { stack: err.stack })
+  });
+};
+
+export default errorHandler;
--- Frontend/src/app/app.ts
+++ Frontend/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatInput} from '@angular/material/input'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatInput],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
