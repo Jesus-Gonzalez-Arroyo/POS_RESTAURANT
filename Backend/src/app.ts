85818e374ee172225f6c43a85bbdf635c626807c
jesus1102 <jesusdavidgonzales1102@gmail.com>
2025-09-25 17:53:21 -0500

    fix(auth): mejora en los endpoints de autenticacion de usuarios

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import router_products  from './routes/products.route';
 import router_users from './routes/user.route';
 import router_auth from './routes/auth.route';
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use('/api/v1/products', router_products);
--- Backend/src/controllers/auth.controller.ts
+++ Backend/src/controllers/auth.controller.ts
@@ -5,7 +5,7 @@ export const login = async (req: Request, res: Response): Promise<void> => {
     try {
         const { user, password } = req.body;
         const result = await loginUser(user, password);
-        res.status(result.status).json({ message: result.message, token: result.token });
+        res.status(result.status).json(result);
     } catch (error) {
         console.error('Error during login:', error);
         res.status(500).json({ message: 'No se pudo iniciar sesión, intente de nuevo' });
