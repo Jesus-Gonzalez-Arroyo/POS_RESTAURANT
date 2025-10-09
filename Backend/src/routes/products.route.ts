fc335fbde6506cad06ff765535918e9fe585b652
jesus1102 <jesusdavidgonzales1102@gmail.com>
2025-10-09 14:21:20 -0500

    feat(service): creacion de los endpoints para gestionar los productos

--- Backend/src/routes/products.route.ts
+++ Backend/src/routes/products.route.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express'
-import { fetchAllProducts } from '../controllers/products.controller'
+import { fetchAllProducts, addNewProduct, updateProduct, deleteProduct } from '../controllers/products.controller'
 import { authenticateToken } from '../middlewares/auth.middleware'
 
 const router_products = Router()
 
 router_products.get('/', authenticateToken, fetchAllProducts)
+router_products.post('/', authenticateToken, addNewProduct)
+router_products.put('/:id', authenticateToken, updateProduct)
+router_products.delete('/:id', authenticateToken, deleteProduct)
 
 export default router_products
