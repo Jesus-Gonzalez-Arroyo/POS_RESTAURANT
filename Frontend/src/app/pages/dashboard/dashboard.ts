c69ad1d21ad876bb4806ba5060672a67482ce21d
jesus1102 <jesusdavidgonzales1102@gmail.com>
2025-11-20 14:42:03 -0500

    fix(changes): cambios en las interfaces y mejoras en las funciones

--- Frontend/src/app/pages/dashboard/dashboard.ts
+++ Frontend/src/app/pages/dashboard/dashboard.ts
@@ -2,6 +2,10 @@ import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
 
+import { Dashboard as DashboardService } from '../../core/services/dashboard/dashboard';
+import { DashboardData, productsTop } from '../../core/models/index';
+import { formatPriceCustom } from '../../shared/utils/formatPrice';
+
 @Component({
   selector: 'app-dashboard',
   imports: [MatIconModule, CommonModule],
@@ -10,15 +14,61 @@ import { CommonModule } from '@angular/common';
   standalone: true
 })
 export class Dashboard {
+  constructor(private dashboardService: DashboardService) {}
+
   orders = [
     { id: 1, customer: 'John Doe', total: 99.99, status: 'Pending', pedido: 'Pollo' },
     { id: 2, customer: 'Jane Smith', total: 149.49, status: 'Completed', pedido: 'Pizza' },
     { id: 3, customer: 'Alice Johnson', total: 79.89, status: 'Cancelled', pedido: 'Sushi' }
   ]
 
-  productsTop = [
-    { id: 1, name: 'Pollo', sales: 150 },
-    { id: 2, name: 'Pizza', sales: 120 },
-    { id: 3, name: 'Sushi', sales: 90 }
-  ]
+  productsTop: productsTop[] = []
+  salesToday: DashboardData['salesToday'] = {
+    total_sales: '0',
+    total_revenue: '0',
+    total_profits: '0',
+    salesCountChange: 0,
+    revenueChange: 0,
+    profitsChange: 0
+  }
+  salesMonth: DashboardData['salesMonth'] = {
+    total_sales: '0',
+    total_revenue: '0',
+    total_profits: '0',
+    salesCountChange: 0,
+    revenueChange: 0,
+    profitsChange: 0
+  }
+  expensesToday: DashboardData['expensesToday'] = {
+    total_expenses: '0',
+    expensesChange: 0
+  }
+  expensesMonth: DashboardData['expensesMonth'] = {
+    total_expenses: '0',
+    expensesChange: 0
+  }
+
+  ngOnInit() {
+    this.loadDashboardData();
+  }
+
+  loadDashboardData() {
+    this.dashboardService.getDashboardData().subscribe({
+      next: (data: any) => {
+        const dataDashboard = data as DashboardData;
+        this.productsTop = dataDashboard.topProducts;
+        this.salesToday = dataDashboard.salesToday;
+        this.salesMonth = dataDashboard.salesMonth;
+        this.expensesToday = dataDashboard.expensesToday;
+        this.expensesMonth = dataDashboard.expensesMonth;
+      },
+      error: (error) => {
+        console.error('Error loading dashboard data:', error);
+      }
+    });
+  }
+
+  modifyMoneyFormat(amount: string): string {
+    return formatPriceCustom(Number(amount));
+  }
 }
